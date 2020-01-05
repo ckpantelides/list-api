19a0813f2a9471e9282d5a98c36047c75b275274
CK Pantelides <ckpantelides@gmail.com>
2020-01-05 11:39:53 +0000

    Added cinema-search route for placename search

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ let filmTimes = mockFilmData;
 app.get(
   "/cinemas",
   function(req, res, next) {
+    let cinemas = [];
     let lat = req.query.lat;
     let lon = req.query.lon;
     // let startUrl = "https://api.list.co.uk/v1/places?near=51.6016,-0.1934/5";
@@ -110,3 +111,54 @@ app.get(
     res.send(req.app.locals.data);
   }
 );
+
+// find cinemas after placename search
+app.get("/cinema-search", function(req, res, callback) {
+  let cinemas = [];
+  // takes search value and geocodes it using google API
+  let newSearch = req.query.searchInput;
+  let googleKey = process.env.googleAPI;
+
+  let geoUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${newSearch}&key=${googleKey}`;
+
+  request(geoUrl, function(err, response, body) {
+    if (err) {
+      console.log("Error with geo location search");
+    }
+    // if geocode a success, search for cinemas
+    else {
+      let geodata = JSON.parse(body);
+      let lat = geodata.results[0].geometry.location.lat;
+      let lon = geodata.results[0].geometry.location.lng;
+
+      // let startUrl = "https://api.list.co.uk/v1/places?near=51.6016,-0.1934/5";
+      let startUrl = `https://api.list.co.uk/v1/places?near=${lat},${lon}/5`;
+      let apiKey = process.env.thelistAPI;
+
+      let opts = {
+        url: startUrl,
+        headers: {
+          Authorization: `Bearer ${apiKey}`
+        }
+      };
+      request(opts, function(err, res, body) {
+        if (err) {
+          console.log(err);
+          process.exit();
+        }
+        body = JSON.parse(body);
+
+        body = body.filter(e => {
+          return e.tags.includes("cinemas");
+        });
+        cinemas = cinemas.concat(body).slice(0, 9);
+        req.app.locals.cinemas = JSON.stringify(cinemas);
+        let searchResults = req.app.locals.cinemas;
+        return callback(searchResults);
+      });
+    }
+  });
+  function callback(data) {
+    res.send(data);
+  }
+});
