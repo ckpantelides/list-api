1d1a4d68fc317c3b5c26c82d387cd70c99ec159a
CK Pantelides <ckpantelides@gmail.com>
2020-05-13 12:31:01 +0100

    Rewritten filmtimeapp route using spread operator

--- routes/filmtimesapp.js
+++ routes/filmtimesapp.js
@@ -28,68 +28,27 @@ router.get('/', function (req, res, next) {
         return e.tags.includes('film');
       });
     }
-    // filmTimes = filmTimes.concat(body);
+    let filmData = filmTimes.concat(body);
 
-    //  const mockFilmData = require("./film-times.json");
-    const mockFilmData = filmTimes.concat(body);
+    // This creates an array trimming the unnecessary film data, leaving just the name
+    // performances and description. I felt this was necessary as the original data array is
+    // complex and has many nested properties
+    let trimmedData = [];
 
-    // The avoid the film data being mutated each time the performances are filter by date
-    // I've created separate copies for each day of the week
-    let trimmedMockData0 = [];
-    let trimmedMockData1 = [];
-    let trimmedMockData2 = [];
-    let trimmedMockData3 = [];
-    let trimmedMockData4 = [];
-    let trimmedMockData5 = [];
-    let trimmedMockData6 = [];
-
-    // This creates an object trimming the unnecessary film data, leaving just the name
-    // and the performances. I felt this was necessary as the original data array is complex and has
-    // many nested properties
-    if (mockFilmData.length > 0 && mockFilmData !== undefined) {
-      for (let i = 0; i < mockFilmData.length; i++) {
-        (trimmedMockData0[i] = {
-          name: mockFilmData[i].name,
-          times: mockFilmData[i].schedules[0].performances,
-          description: mockFilmData[i].descriptions[0].description,
-        }),
-          (trimmedMockData1[i] = {
-            name: mockFilmData[i].name,
-            times: mockFilmData[i].schedules[0].performances,
-            description: mockFilmData[i].descriptions[0].description,
-          }),
-          (trimmedMockData2[i] = {
-            name: mockFilmData[i].name,
-            times: mockFilmData[i].schedules[0].performances,
-            description: mockFilmData[i].descriptions[0].description,
-          }),
-          (trimmedMockData3[i] = {
-            name: mockFilmData[i].name,
-            times: mockFilmData[i].schedules[0].performances,
-            description: mockFilmData[i].descriptions[0].description,
-          }),
-          (trimmedMockData4[i] = {
-            name: mockFilmData[i].name,
-            times: mockFilmData[i].schedules[0].performances,
-            description: mockFilmData[i].descriptions[0].description,
-          }),
-          (trimmedMockData5[i] = {
-            name: mockFilmData[i].name,
-            times: mockFilmData[i].schedules[0].performances,
-            description: mockFilmData[i].descriptions[0].description,
-          }),
-          (trimmedMockData6[i] = {
-            name: mockFilmData[i].name,
-            times: mockFilmData[i].schedules[0].performances,
-            description: mockFilmData[i].descriptions[0].description,
-          });
+    if (filmData.length > 0 && filmData !== undefined) {
+      for (let i = 0; i < filmData.length; i++) {
+        trimmedData[i] = {
+          name: filmData[i].name,
+          times: filmData[i].schedules[0].performances,
+          description: filmData[i].descriptions[0].description,
+        };
       }
     }
 
     // this function returns the films by day of the week. In the original data,
     // each film is an object, and all of the week's perfomances of that film are stored in
     // an array of objects
-    function factoryTrim(filmTimes, screeningDay) {
+    function trimByDay(filmTimes, screeningDay) {
       let formattedDate = screeningDay.toISOString().slice(0, 10);
 
       // only include performances that match the relevant date (above)
@@ -118,14 +77,15 @@ router.get('/', function (req, res, next) {
     let longDate5 = new Date(new Date().getTime() + 5 * 24 * 60 * 60 * 1000);
     let longDate6 = new Date(new Date().getTime() + 6 * 24 * 60 * 60 * 1000);
 
-    if (mockFilmData.length > 0 && mockFilmData !== undefined) {
-      let day0 = factoryTrim(trimmedMockData0, longDate0);
-      let day1 = factoryTrim(trimmedMockData1, longDate1);
-      let day2 = factoryTrim(trimmedMockData2, longDate2);
-      let day3 = factoryTrim(trimmedMockData3, longDate3);
-      let day4 = factoryTrim(trimmedMockData4, longDate4);
-      let day5 = factoryTrim(trimmedMockData5, longDate5);
-      let day6 = factoryTrim(trimmedMockData6, longDate6);
+    if (filmData.length > 0 && filmData !== undefined) {
+      // The spread operator is used to clone the trimmedData, so it's not mutated by each function call
+      let day0 = trimByDay([...trimmedData], longDate0);
+      let day1 = trimByDay([...trimmedData], longDate1);
+      let day2 = trimByDay([...trimmedData], longDate2);
+      let day3 = trimByDay([...trimmedData], longDate3);
+      let day4 = trimByDay([...trimmedData], longDate4);
+      let day5 = trimByDay([...trimmedData], longDate5);
+      let day6 = trimByDay([...trimmedData], longDate6);
 
       // An array storing all of the film data to be sent to the front end
       let collatedFilmsByDay = [day0, day1, day2, day3, day4, day5, day6];
